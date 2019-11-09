2bb819dba3378c3c8a1cffbae4c7c4989263a81b
Raunaq Sharma <raushar1@in.ibm.com>
2019-11-09 13:26:25 +0530

    Header Added

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AirlinelistcomponentComponent } from './airlinelistcomponent/airlinelistcomponent.component';
+
+
+
+@NgModule({
+  declarations: [AirlinelistcomponentComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
