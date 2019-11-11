318d742cf7dddd08325763e6249b2d834d8e6e9c
Archit Goyal <architcool09@gmail.com>
2019-11-11 10:03:50 +0530

    Sign up component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user/user.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegistrationComponent } from './registration/registration.component';
+
+
+
+
+@NgModule({
+  declarations: [RegistrationComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class UserModule { }
