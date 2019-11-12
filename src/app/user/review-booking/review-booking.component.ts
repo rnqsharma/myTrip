bbcc9406aa05c8eadc0a74d84a8c5053fc1f05dd
Sandhya Rani Nagamallu <sannaga1@in.ibm.com>
2019-11-12 09:12:49 +0530

    Footer Added

---
+++ src/app/user/review-booking/review-booking.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-review-booking',
+  templateUrl: './review-booking.component.html',
+  styleUrls: ['./review-booking.component.css']
+})
+export class ReviewBookingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
